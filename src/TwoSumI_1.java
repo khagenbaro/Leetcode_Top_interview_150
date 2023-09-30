60974ab8d2a647dedfa70a27c5dbce7fb068eb4f
khaegenm1 <khagenbaro1997@gmail.com>
2023-09-30 20:11:55 +0530

    two sum using hash map

---
+++ src/TwoSumI_1.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumI_1 {
+    public static void main(String[] args) {
+        int[] arr = {2,7,11,15};
+        int res[] = twoSum(arr,17);
+        System.out.println(Arrays.toString(res));
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> intMap = new HashMap<>();
+        for(int i =0;i<nums.length;i++){
+            intMap.put(nums[i],i);
+        }
+        for(int i =0;i<nums.length;i++){
+            int complement = target - nums[i];
+            if(intMap.containsKey(complement) && intMap.get(complement)!= i){
+                return new int[]{i, intMap.get(complement)};
+            }
+
+        }
+        return null;
+    }
+
+}
+/*1. Two Sum
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]*/
